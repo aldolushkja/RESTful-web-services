e57f6ca2f6740b5c756ddced168ccd9cf9f8a0c9
firetaurus <aldo.lushkja@gmail.com>
2018-11-21 22:44:17 +0100

    added UserBean and UserService

--- src/main/java/com/alushkja/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/alushkja/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -1,4 +1,4 @@
-package com.alushkja.rest.webservices.restfulwebservices;
+package com.alushkja.rest.webservices.restfulwebservices.helloworld;
 
 public class HelloWorldBean {
 
--- src/main/java/com/alushkja/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/alushkja/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.alushkja.rest.webservices.restfulwebservices;
+package com.alushkja.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
