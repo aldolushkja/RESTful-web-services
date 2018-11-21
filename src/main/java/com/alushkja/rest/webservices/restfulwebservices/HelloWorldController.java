1bd0fb8ff6563e1d6761343a6ca4d384b83eeeac
firetaurus <aldo.lushkja@gmail.com>
2018-11-21 22:26:43 +0100

    enchancing hello world service with path variable, added loggin level debug

--- src/main/java/com/alushkja/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/alushkja/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.alushkja.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,15 @@ public class HelloWorldController {
 	}
 	
 	//GET
-	//URI - hello-world-bean
+	//URI - /hello-world-bean
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello world");
+	}
+	
+	//URI - /hello-world/path-variable/firetaurus
+	@GetMapping(path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}
 }
