68d29e4d3d1cc50baf8767d292212605b0b308e0
firetaurus <aldo.lushkja@gmail.com>
2018-11-26 15:09:56 +0100

    Enhancing Swagger Documentation with Custom Annotations

--- src/main/java/com/alushkja/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/alushkja/rest/webservices/restfulwebservices/user/User.java
@@ -9,14 +9,20 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.mapping.Array;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about the user.")
 public class User {
 
 	private Integer id;
 
 	@Size(min=2, message="Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Name should have atleast 2 characters")
 	private String name;
 
 	@Past
+	@ApiModelProperty(notes="Birth date should be on the past")
 	private Date birthDate;
 
 	private List<Post> posts = new ArrayList<>();
